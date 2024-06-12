62e0696a6167d64ac7154f24a208b9a24a4d14a8
rfdzan <hekodev.py@gmail.com>
2024-06-12 20:10:30 +0700

    (wip) fixing node swapping

--- src/Main.scala
+++ src/Main.scala
@@ -2,47 +2,62 @@ package Main
 import scala.collection.mutable.Map
 
 case class MapValue[T](
-    private val value: T,
-    private val node: Node[T]
+    val value: T,
+    var node: Node[T]
 ):
   def getVal: T =
     return value
-  def getNode: Node[T] =
-    return node
 
 class lruCache[T](itemLimit: Int):
   private var map: Map[String, MapValue[T]] = Map()
   private var dll = new doublyLinkedList[String]
-  private def evict(key: String, put: Boolean): Unit =
-    val plusOne = if put then 1 else 0
-    if map.size + plusOne > itemLimit then
+  private def evict(key: String): Boolean =
+    if map.size + 1 > itemLimit then
       // get key from ddl
       // use popped key from ddl to
       // remove map entry possesing that key.
-      val popped = dll.popFront match
-        case None      => None
-        case Some(key) => 
+      dll.popFront match
+        case None => return false
+        case Some(key) =>
           map.remove(key)
-          dll.pushToBack(key)
+          return true
+    return false
+  private def mostRecentlyUsed(key: String, mapVal: MapValue[T]) =
     // TODO: move cache hit keys to the end of the list
+    val storedNodeNextNode = mapVal.node.nextNode
+    val storedNodePrevNode = mapVal.node.prevNode
+    mapVal.node.nextNode match
+      case None => None
+      case Some(node) =>
+        node.prevNode = storedNodePrevNode
+    mapVal.node.prevNode match
+      case None => None
+      case Some(node) =>
+        node.nextNode = storedNodeNextNode
+    map.update(key, mapVal)
+  def readQueue =
+    dll.readAll
   def has(key: String): Boolean =
     map.get(key) match
       case None        => false
-      case Some(value) => 
-        evict(key, false)
+      case Some(value) =>
+        // this is probably not passing by reference
+        mostRecentlyUsed(key, value)
+        // dll.pushToBack(Node(key))
         true
   def get(key: String): Option[T] =
-    val mapVal = map.get(key) match
-      case None => 
+    map.get(key) match
+      case None =>
         // cache miss
-        None
+        return None
       case Some(value) =>
         // cache hit
-        evict(key, false)
-        Some(value.getVal)
-    return mapVal
+        mostRecentlyUsed(key, value)
+        // dll.pushToBack(Node(key))
+        return Some(value.getVal)
   def set(key: String, value: T): Unit =
-    evict(key, true)
+    evict(key)
+    dll.pushToBack(Node(key))
     map.addOne(key, MapValue(value, Node(value)))
 
 class Node[T](data: T):
@@ -58,30 +73,41 @@ class doublyLinkedList[T](
     var firstNode: Option[Node[T]] = None,
     var lastNode: Option[Node[T]] = None
 ):
-  def pushToFront(data: T): Unit =
-    val newNode = Some(Node(data))
+  def readAll: Unit =
+    var currentNode = firstNode
+    while (true)
+      println(currentNode match
+        case None        => return
+        case Some(value) => Some(value.content)
+      )
+      currentNode match
+        case None => return
+        case Some(value) =>
+          currentNode = value.nextNode
+  def pushToFront(newNode: Node[T]): Option[Node[T]] =
     if firstNode == None then
-      firstNode = newNode
-      lastNode = newNode
-      return
-    newNode match
-      case Some(node) => node.addNextNode(firstNode)
+      firstNode = Some(newNode)
+      lastNode = Some(newNode)
+      return Some(newNode)
+    newNode.addNextNode(firstNode)
     firstNode match
-      case None       => return
-      case Some(node) => node.addPreviousNode(newNode)
-    firstNode = newNode
-  def pushToBack(data: T): Unit =
-    val newNode = Some(Node(data))
+      case None       => return None
+      case Some(node) => node.addPreviousNode(Some(newNode))
+    firstNode = Some(newNode)
+    return Some(newNode)
+
+  def pushToBack(newNode: Node[T]): Option[Node[T]] =
     if firstNode == None then
-      firstNode = newNode
-      lastNode = newNode
-      return
-    newNode match
-      case Some(node) => node.addPreviousNode(lastNode)
+      firstNode = Some(newNode)
+      lastNode = Some(newNode)
+      return Some(newNode)
+    newNode.addPreviousNode(lastNode)
     lastNode match
-      case None       => return
-      case Some(node) => node.addNextNode(newNode)
-    lastNode = newNode
+      case None       => return None
+      case Some(node) => node.addNextNode(Some(newNode))
+    lastNode = Some(newNode)
+    return Some(newNode)
+
   def popFront: Option[T] =
     val toReturn = firstNode match
       case None       => None
@@ -103,4 +129,15 @@ class doublyLinkedList[T](
     return toReturn
 
 @main def main =
+  val newLru = lruCache[Int](3)
+  newLru.set("one", 1)
+  newLru.set("two", 2)
+  newLru.set("three", 3)
+
+  newLru.readQueue
   println()
+  newLru.has("two")
+  newLru.readQueue
+  // assert(newLru.get("two") == None)
+  // assert(newLru.get("three") == Some(3))
+  // assert(newLru.get("four") == Some(4))
